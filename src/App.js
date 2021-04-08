470b90147b2a57a128a9df4a55f18a3da0ed6c5b
hanyseyedy <hanyseyedy@gmail.com>
2021-04-08 04:31:25 +0430

    for fire push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import TodoItem from "./components/TodoItem";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="todo-list">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </div>
+  );
 }
 
 export default App;
